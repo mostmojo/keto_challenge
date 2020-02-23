c4887f5f5cba0e8e720b46a2dc8a14d42c42104a
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-02-23 22:38:39 +0200

    Make ES6 JS to ES5 to prevent gulp error and update README file

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# KETO
+# KETO Challenge
 
 ## Usage
 
-### Advanced Usage
+1. Run `npm install`
+2. Next, run `npm start`
 
-run `npm install`
-next run `npm start`
-You can view the `gulpfile.js` to see which tasks are included with the dev environment.
+-----
 
-You must have npm installed globally in order to use this build environment.
+* You can view the `gulpfile.js` to see which tasks are included with the dev environment.
+* You must have npm installed globally in order to use this build environment.
+
+_____
+
+**Note**
+
+* The [geoplugin](http://www.geoplugin.net) free API was used to track user IP for location.
+* The free version only works with `http` so should render the results in local environment, but will not when pushed to github as this hosting contains `https`.
+* Please run on `localhost` to see [result](https://res.cloudinary.com/mostmojo/image/upload/v1582490238/Screenshot_2020-02-23_at_10.36.39_PM.png) in navigation banner 😊
--- js/scripts.js
+++ js/scripts.js
@@ -118,22 +118,21 @@ function createCookie(name, value, expires) {
 // Timer to boot the 10 minute countdown
 
 function timerBoot(duration, display) {
-	let timer = duration;
-	let minutes;
-	let seconds;
-	setInterval(() => {
-			minutes = parseInt(timer / 60, 10);
-			seconds = parseInt(timer % 60, 10);
-			minutes = minutes < 10 ? `0${minutes}` : minutes;
-			seconds = seconds < 10 ? `0${seconds}` : seconds;
-			display.textContent = `${minutes}:${seconds}`;
-			if (--timer < 0) {
-					timer = duration;
+	var countdown = duration,
+			mins, secs;
+	setInterval(function() {
+			mins = parseInt(countdown / 60, 10);
+			secs = parseInt(countdown % 60, 10);
+			mins = mins < 10 ? "0" + mins : mins;
+			secs = secs < 10 ? "0" + secs : secs;
+			display.textContent = mins + ":" + secs;
+			if (--countdown < 0) {
+					countdown = duration;
 			}
 	}, 1000);
 }
-window.onload = () => {
-	const tenMinutes = 60 * 10;
-	const display = document.querySelector('#countdown');
-	timerBoot(tenMinutes, display);
+window.onload = function() {
+	var tenMins = 60 * 10,
+			display = document.querySelector('#countdown');
+	timerBoot(tenMins, display);
 };
